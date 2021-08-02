01e2186d7502cf021de13a77a1004ebfeef7b87a
Tim Hor <timothy.hor@outlook.com>
2021-08-02 13:02:49 +1000

    Add function to fetch latest data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import { Location } from './types';
+
+async function fetchData() {
+  const METADATA_URL =
+    'https://data.nsw.gov.au/data/api/3/action/package_show?id=0a52e6c1-bc0b-48af-8b45-d791a6d8e289';
+  try {
+    const metadataResponse = await axios.get(METADATA_URL);
+    const jsonResource = metadataResponse.data.result.resources.find(
+      (resource: any) => resource.format === 'JSON'
+    );
+    const dataResponse = await axios.get(jsonResource.url);
+    return dataResponse.data;
+  } catch {
+    throw new Error('Failed to fetch data');
+  }
+}
 
 function App() {
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+    fetchData().then((rawData: any) => {
+      setLocations(rawData.data.monitor);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {locations.map((location: Location, index: number) => {
+        return <pre key={index}>{`${location.Venue} | ${location.Date}`}</pre>;
+      })}
     </div>
   );
 }
---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export type Location = {
+  Venue: string;
+  Address: string;
+  Suburb: string;
+  Date: string;
+  Time: string;
+  Alert: string;
+  Lon: string;
+  Lat: string;
+  HealthAdviceHTML: string;
+  'Last updated date': string;
+  transmissionvenues: string;
+};
