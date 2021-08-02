b0ccf2ba9686968aee7a2732ac7c0d3a318eebb9
Tim Hor <timothy.hor@outlook.com>
2021-08-02 16:48:18 +1000

    Show different marker colours for different alert types

--- src/App/MapContainer/MapContainer.tsx
+++ src/App/MapContainer/MapContainer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useJsApiLoader, GoogleMap, Marker } from '@react-google-maps/api';
 import { Location } from '../types';
+import { AlertType, getAlertType } from '../utils';
 
 const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
@@ -14,20 +15,64 @@ const center = {
   lng: 151.04,
 };
 
+function getIcon(alert: string) {
+  const ALERT_ICON_COMMON = {
+    path: google.maps.SymbolPath.CIRCLE,
+    scale: 6,
+    strokeColor: 'white',
+    strokeWeight: 2,
+    fillOpacity: 1.0,
+  };
+
+  const alertType = getAlertType(alert);
+  switch (alertType) {
+    case AlertType.CLOSE:
+      return {
+        ...ALERT_ICON_COMMON,
+        fillColor: 'red',
+      };
+    case AlertType.ADVICE:
+      return {
+        ...ALERT_ICON_COMMON,
+        fillColor: 'brown',
+      };
+    case AlertType.CASUAL:
+      return {
+        ...ALERT_ICON_COMMON,
+        fillColor: 'orange',
+      };
+    case AlertType.MONITOR:
+      return {
+        ...ALERT_ICON_COMMON,
+        fillColor: '#0398fc',
+      };
+    default:
+      return {
+        ...ALERT_ICON_COMMON,
+        fillColor: 'black',
+      };
+  }
+}
+
+function generateMarker(location: Location, index: number) {
+  const position = { lat: Number(location.Lat), lng: Number(location.Lon) };
+  const icon = getIcon(location.Alert);
+  return <Marker key={index} position={position} icon={icon} />;
+}
+
 function MapContainer({ locations }: { locations: Location[] }) {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: apiKey!,
   });
 
+  const markers = locations.map((location, index) =>
+    generateMarker(location, index)
+  );
+
   return isLoaded ? (
     <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={11}>
-      {locations.map((location, index) => (
-        <Marker
-          key={index}
-          position={{ lat: Number(location.Lat), lng: Number(location.Lon) }}
-        />
-      ))}
+      {markers}
     </GoogleMap>
   ) : null;
 }
---
+++ src/App/utils.ts
@@ -0,0 +1,23 @@
+export enum AlertType {
+  CLOSE,
+  ADVICE,
+  CASUAL,
+  MONITOR,
+  UNKNOWN,
+}
+
+export function getAlertType(alert: string) {
+  if (alert.includes('14 days')) {
+    return AlertType.CLOSE;
+  }
+  if (alert.includes('further advice')) {
+    return AlertType.ADVICE;
+  }
+  if (alert.includes('negative result')) {
+    return AlertType.CASUAL;
+  }
+  if (alert.includes('Monitor for symptoms')) {
+    return AlertType.MONITOR;
+  }
+  return AlertType.UNKNOWN;
+}
